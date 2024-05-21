cef4a0052e30eecc853a83aa1306f5e3ff65c9d6
NaaQuinteiro <thaynaquinteiro@gmail.com>
2024-05-21 16:33:23 -0300

    Localização em tempo real maps

--- src/telas/Inicial.tsx
+++ src/telas/Inicial.tsx
@@ -1,28 +1,84 @@
-//AQUI FICA A FUNÇÃO PARA GERAR O COMPONENTE (TELAS)
 
-//importando os componentes, todos os componentes que usamos é preciso importál-os 
-import { ImageBackground, StyleSheet, View } from "react-native";
+import {StyleSheet, View } from "react-native";
 import { Cabecalho } from "../componentes/Cabecalho";
-import{ Botao } from "../componentes/Botao"
+import { useEffect, useRef, useState } from "react";
+
+// Biblioteca para o usuario permitir acesso a localização 
+// obtém a localização do usuario 
+import { 
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+    LocationObject,
+    watchPositionAsync, //observa a posição
+    LocationAccuracy
+
+ } from 'expo-location';
+
+ import MapView, { Marker} from "react-native-maps";
+
 
 export function Inicial(){
+
+    const [location, setLocation] = useState<LocationObject | null>(null);
+
+    const mapRef = useRef<MapView>(null);
+
+    async function requestLocationPermissions() {
+        const { granted } = await requestForegroundPermissionsAsync();
+
+        if (granted) {
+            const currentPosition = await getCurrentPositionAsync();
+            setLocation(currentPosition); // salva a região inicial do usuario no mapa
+        }
+    }
+
+    //executa assim que a tela é reinderizada
+    useEffect(() => {
+        requestLocationPermissions();
+    }, []);
+
+    useEffect(() => {
+        watchPositionAsync({
+            accuracy: LocationAccuracy.Highest,
+            timeInterval: 1000,
+            distanceInterval: 1
+        }, (response)=> {
+            setLocation(response);
+
+            mapRef.current?.animateCamera({
+                pitch: 70,
+                center: response.coords
+            });
+        });
+    }, []);
+
     return(
         <View style={styles.container}> 
             <Cabecalho 
                 titulo="HOME"
             />  
+            
+            { 
+                location && 
+                <MapView
+                    ref={mapRef}
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                        latitudeDelta: 0.005,
+                        longitudeDelta: 0.005
+                    }}
+                >
+                    <Marker
+                        coordinate={{
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude,
+                        }}
+                    />
+                </MapView>
 
-            <ImageBackground
-                style={styles.fundo}
-                source={require('../../assets/mapa.png')}
-            > 
-
-                <Botao texto='Teste '/>
-                <Botao texto='Teste '/>
-                <Botao texto='Teste '/>
-                <Botao texto='Teste '/>
-
-            </ImageBackground>     
+            }
 
         </View>
     );
@@ -36,11 +92,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#000000' 
     },
 
-    fundo: {
+    map: {
        flex: 1,
-       flexDirection: 'row',
-       flexWrap: 'wrap',
-       resizeMode: 'cover',
+       width: '100%',
     },
 
 });
